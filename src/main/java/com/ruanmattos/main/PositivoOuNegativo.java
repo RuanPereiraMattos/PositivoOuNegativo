23f514ee5ec2f19e8a323662ac687e750fedf14e
Ruan <ruandupg@gmail.com>
2024-03-01 21:22:47 -0300

    Primeira atualização do programa implementando a verificação de positivo ou negativo

--- src/main/java/com/ruanmattos/main/PositivoOuNegativo.java
+++ src/main/java/com/ruanmattos/main/PositivoOuNegativo.java
@@ -4,13 +4,27 @@
 
 package com.ruanmattos.main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author aluno
  */
 public class PositivoOuNegativo {
 
+    /**
+     * Programa que verifica se o número digitado pelo usuário é positivo ou negativo
+     * @param args 
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bem vindo(a) ao programa que irá lhe dizer se o número que você digitar é Positivo ou Negativo");
+        System.out.print("Digite o número que deseja verificar: ");
+        Scanner s = new Scanner(System.in);
+        double d = s.nextDouble();
+        if(0 <= d){
+            System.out.println("O número digitado é positivo");
+        }else{
+            System.out.println("O número digitado é negativo");
+        }
     }
 }
