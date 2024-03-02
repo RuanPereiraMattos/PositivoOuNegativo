fbfafc304888e051abde142862cf0e18fd6d5642
Ruan <ruandupg@gmail.com>
2024-03-01 21:17:26 -0300

    Primeira versão

---
+++ src/main/java/com/ruanmattos/main/PositivoOuNegativo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ruanmattos.main;
+
+/**
+ *
+ * @author aluno
+ */
+public class PositivoOuNegativo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
